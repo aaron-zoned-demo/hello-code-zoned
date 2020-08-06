f49ac380da044efaf6f93355d7ac6129757d8e45
aburd <burdick.aaron@gmail.com>
2020-08-06 20:23:25 +0900

    add history

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useHistory } from 'react-router'
 
 export default function Login() {
   return (
     <div className="Login">
-      <button className="btn-success">Login with Gmail</button>
+      <button
+        className="btn-success"
+      >
+        Login with Gmail
+      </button>
     </div>
   )
 }
